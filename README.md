ac63a190d9f5e7b2cda8cbd4c94d47fe9c3614a1
Florian Kutschera <florian.kutschera@conceptboard.com>
2017-01-19 09:53:39 +0100

    add moment dependency and bump version number

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MMM-EFA-departures
 MagicMirror² module to show departures for public transport stations using the EFA system.
+
+e.g.: efa-bw.de, 
--- node_helper.js
+++ node_helper.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const moment = require('moment');
 const NodeHelper = require("node_helper");
 
 module.exports = NodeHelper.create({
@@ -10,12 +11,15 @@ module.exports = NodeHelper.create({
     socketNotificationReceived: function(notification, payload) {
         if(notification === 'CONFIG'){
             this.config = payload;
+
             var efa_url = this.config.efaUrl;
                 efa_url += '&name_dm=' + this.config.stopID;
                 efa_url += '&type_dm=any';
                 efa_url += '&line=' + this.config.lines.join('&line=');
                 efa_url += '&outputFormat=json';
                 efa_url += '&limit=' + this.config.maxDepartures;
+                efa_url += '&itdTime=' + moment().format('HHmm');
+                efa_url += '&itdDate=' + moment().format('YYYYMMDD');
             console.log(efa_url);
             this.getData(efa_url, this.config.stopID);
         }
